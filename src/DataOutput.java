b5600866155832c528123ce73b015a382f68d20c
roman <romanzchov@mail.ru>
2023-12-18 22:36:58 +0300

    Home work 12 finish

--- src/DataOutput.java
+++ src/DataOutput.java
@@ -6,7 +6,7 @@ public class DataOutput {
         System.out.println(bulgakov.getName() + " " + book.getBookName() + book.getPublishingYear());
         Author tolstoy = new Author("Лев Толстой");
         Book book1 = new Book(tolstoy, "Анна Каренина ", 1950);
-        book1.setPublishingYear(2001);
+        book1.setPublishingYear(2002);
         System.out.println(tolstoy.getName() + " " + book1.getBookName() + " " + book1.getPublishingYear());
     }
 
