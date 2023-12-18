16a27527d91bf58ed0e5841bf75bf089ec15c051
roman <romanzchov@mail.ru>
2023-12-18 22:36:00 +0300

    Home work 12 finish

---
+++ src/Book.java
@@ -0,0 +1,22 @@
+public class Book {
+    private Author author;
+    private String bookName;
+    private int publishingYear;
+
+    public Book(Author author, String bookName, int publishingYear) {
+        this.bookName = bookName;
+        this.publishingYear = publishingYear;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+}
---
+++ src/DataOutput.java
@@ -0,0 +1,13 @@
+public class DataOutput {
+    public static void main(String[] args) {
+        Author bulgakov = new Author("Михаил Булгаков");
+        Book book = new Book(bulgakov, " Мастер и Маргарита ", 2015);
+        book.setPublishingYear(2018);
+        System.out.println(bulgakov.getName() + " " + book.getBookName() + book.getPublishingYear());
+        Author tolstoy = new Author("Лев Толстой");
+        Book book1 = new Book(tolstoy, "Анна Каренина ", 1950);
+        book1.setPublishingYear(2001);
+        System.out.println(tolstoy.getName() + " " + book1.getBookName() + " " + book1.getPublishingYear());
+    }
+
+}
